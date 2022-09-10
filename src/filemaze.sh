a065f41731648a5ee23ef5ac1c70b7f0ee110f3f
xidream <verycool3580@gmail.com>
2022-09-10 23:01:18 +0800

    feat: init arg -d|--depth and improvements

--- src/filemaze.sh
+++ src/filemaze.sh
@@ -1,19 +1,45 @@
-default_depth=30
+default_depth=16
 
 if [ -z $1 ]
 then
     echo "missing command"
+    exit 1
 fi
 
 case $1 in
     init)
-        if [ -z $2 ]
+        shift
+        while [[ $# -gt 0 ]]; do
+            case $1 in
+                -d|--depth)
+                    if [[ ! $(($2)) -gt 0 ]]
+                    then
+                        echo "invalid depth: $2"
+                        exit 1
+                    fi
+                    depth=$2
+                    shift
+                    shift
+                    ;;
+                *)
+                    echo "unknown option: $1"
+                    exit 1
+                    ;;
+            esac
+        done
+ 
+        if [ -z $depth ]
         then
-            read -p "Enter depth [$default_depth]:" depth
+            read -p "enter depth [$default_depth]:" depth
             depth=${depth:-$default_depth}
-        else
-            depth=$2
         fi
-        echo "depth: $((depth))"
+
+        ;;
+    *)
+        echo "unknown command: $1"
+        exit 1
         ;;
 esac
+
+
+echo "depth: $depth"
